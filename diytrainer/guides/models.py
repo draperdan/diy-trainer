e000b802b9d6602784dbdde1553a857084ab828a
Patrick Beeson <patrickbeeson@gmail.com>
2014-06-18 10:40:14 -0400

    Add guide models for guide and feedback.

--- diytrainer/guides/models.py
+++ diytrainer/guides/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class Guide(models.Model):
+    """ A guide for a given splash page """
+    name = models.CharField(max_length=100, help_text='Limited to 100 characters.')
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Guides'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Feedback(models.Model):
+    """ Feedback submitted for a given guide """
+    guide = models.ForeignKey(Guide)
+    submission_date = models.DateTimeField(default=datetime.datetime.now)
+    email = models.EmailField()
+    project_recommendation = models.TextField()
+    skill_ranking = models.PositiveSmallIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Feedback'
+        ordering = ('submission_date',)
+
+    def __str__(self):
+        return self.id
