64bcdf916ada8b02526c49448076dff4234ab483
Patrick Beeson <patrickbeeson@gmail.com>
2014-06-18 11:31:29 -0400

    Add admin to guide and feedback.

--- diytrainer/guides/admin.py
+++ diytrainer/guides/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Guide, Feedback
+
+
+class GuideAdmin(admin.ModelAdmin):
+    pass
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Guide, GuideAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
