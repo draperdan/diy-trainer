6134eee5c25a66553ec38bd2d038bc54e35c1701
Patrick Beeson <patrickbeeson@gmail.com>
2014-07-03 09:36:10 -0400

    Add homepage template in case folks go to root url. Add code to error templates. Add css for home and error display. Update urls.

--- diytrainer/diytrainer/urls.py
+++ diytrainer/diytrainer/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from .views import RobotsView
 
@@ -8,7 +9,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 
     url(r'^robots\.txt$', RobotsView.as_view()),
     url(r'^', include('guides.urls')),
