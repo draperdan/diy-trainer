c2cf5244e276a76613d8a343d94147b72e2d69f8
Patrick Beeson <patrickbeeson@gmail.com>
2014-07-03 10:20:52 -0400

    Fix import issue in settings.

--- diytrainer/diytrainer/settings/projects_production.py
+++ diytrainer/diytrainer/settings/projects_production.py
@@ -1,25 +1,8 @@
-"""Production settings and globals."""
-
-""" For app served to projects.diy-trainer.com """
+"""Production settings and globals for projects.diy-trainer.com."""
 
 from __future__ import absolute_import
 
-from os import environ
-
 from .production import *
-
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
 
 SITE_ID = 2
 
