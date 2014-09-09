b17f0ca4c6117197021262c2cd9b8ace50ccb6ac
Patrick Beeson <patrickbeeson@gmail.com>
2014-09-09 11:32:49 -0400

    Remove settings from git history and update gitignore.

--- diytrainer/diytrainer/settings/preview_production.py
+++
@@ -1,9 +0,0 @@
-"""Production settings and globals for preview.diy-trainer.com."""
-
-from __future__ import absolute_import
-
-from .production import *
-
-SITE_ID = 3
-
-ROOT_URLCONF = 'preview_urls'
--- diytrainer/diytrainer/settings/projects_production.py
+++
@@ -1,9 +0,0 @@
-"""Production settings and globals for projects.diy-trainer.com."""
-
-from __future__ import absolute_import
-
-from .production import *
-
-SITE_ID = 2
-
-ROOT_URLCONF = 'projects_urls'
